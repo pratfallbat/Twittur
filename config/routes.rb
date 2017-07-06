c7f730950c56a357a92ab0f125998f2baf5ca099
Pratyush kumar <pratyush845@gmail.com>
2017-07-07 02:28:06 +0530

    app built

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+   resources :posts
+  #Define root url
+root 'pages#index'
+
+  get '/home' => 'pages#home'
+
+  get '/user/:id' => 'pages#profile'
+
+  get '/explore' => 'pages#explore'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
